1e873e408aeaa1c8b20e1e4a2ed2d2a4db570de5
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-07-25 14:21:49 +0900

    style: :sparkles:  add prettier

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './style.css';
-import liff from '@line/liff';
+import "./style.css";
+import liff from "@line/liff";
 
-const app = document.querySelector<HTMLDivElement>('#app') as HTMLDivElement;
+const app = document.querySelector<HTMLDivElement>("#app") as HTMLDivElement;
 
 liff
   .init({
-    liffId: import.meta.env.VITE_LIFF_ID
+    liffId: import.meta.env.VITE_LIFF_ID,
   })
   .then(() => {
     app.innerHTML = `
